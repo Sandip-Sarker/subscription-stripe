53d4ceddac07ad466c0f99f2ca273526a720fcf5
Sandip-Sarker <sandipsarke727@gmail.com>
2025-09-19 18:31:57 +0600

    view page

---
+++ app/Http/Controllers/PlanController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PlanController extends Controller
+{
+    public function index()
+    {
+        // Logic to list all plans
+    }
+
+    public function create()
+    {
+        // Logic to show form to create a new plan
+    }
+
+    public function store(Request $request)
+    {
+        // Logic to store a new plan
+    }
+
+    public function show($id)
+    {
+        // Logic to show a specific plan
+    }
+
+    public function edit($id)
+    {
+        // Logic to edit a specific plan
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update a specific plan
+    }
+
+    public function destroy($id)
+    {
+        // Logic to delete a specific plan
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PlanController;
+use App\Http\Controllers\SubscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,23 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Plans
+    Route::get('/plan', [PlanController::class, 'index'])->name('paln.index');
+    Route::get('/plan/create', [PlanController::class, 'create'])->name('paln.create');
+    Route::post('/plan/store', [PlanController::class, 'store'])->name('paln.store');
+    Route::get('/plan/edit/{id}', [PlanController::class, 'edit'])->name('paln.edit');
+    Route::put('/plan/update/{id}', [PlanController::class, 'update'])->name('paln.update');
+    Route::delete('/plan/delete/{id}', [PlanController::class, 'destroy'])->name('paln.delete');
+
+    // Subscriptions
+    Route::get('/subscription', [SubscriptionController::class, 'index'])->name('subscription.index');
+    Route::get('/subscription/create', [SubscriptionController::class, 'create'])->name('subscription.create');
+    Route::post('/subscription/store', [SubscriptionController::class, 'store'])->name('subscription.store');
+    Route::get('/subscription/edit/{id}', [SubscriptionController::class, 'edit'])->name('subscription.edit');
+    Route::put('/subscription/update/{id}', [SubscriptionController::class, 'update'])->name('subscription.update');
+    Route::delete('/subscription/delete/{id}', [SubscriptionController::class, 'destroy'])->name('subscription.delete');
+
 });
 
 require __DIR__.'/auth.php';
